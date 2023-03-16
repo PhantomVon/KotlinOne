049ad6e4650db7a5649d5111b000b1a412ebbf2f
PhantomVon <p9691402@gmail.com>
2023-03-16 13:10:49 +0300

    first commit

---
+++ src/main/kotlin/inbuiltFunctions.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+    var greeting = "Hello World"
+    println(greeting.uppercase())
+    println(greeting.lowercase())
+    println(greeting.length)
+    println(greeting.reversed())
+
+    var myNum = 16.0
+    println(Math.sqrt(myNum))
+    print(Math.pow(myNum, 2.0))
+}
